26ad5c49daaebcb09cb0cae875a8effaeca5f409
Kot4ro <108230041+Kot4ro@users.noreply.github.com>
2023-06-17 15:00:50 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,15 +11,15 @@
 <body>
         <div class="tab">
             <ul class="tab_menu">
-                <li class="tab_menu-item is-active" data-tab="Select">入力</li>
-                <li class="tab_menu-item" data-tab="Graph">グラフ</li>
-                <li class="tab_menu-item" data-tab="Week">週</li>
-                <li class="tab_menu-item" data-tab="Month">月</li>
+                <li class="tab_menu_item is_active" data-tab="Select">入力</li>
+                <li class="tab_menu_item" data-tab="Graph">グラフ</li>
+                <li class="tab_menu_item" data-tab="Week">週</li>
+                <li class="tab_menu_item" data-tab="Month">月</li>
             </ul>
 
 
         <div class="tab_panel">
-            <div class="tab_panel-box tab_panel-box001 is-show" data-tab="Select">
+            <div class="tab_panel_box tab_panel_box001 is_show" data-tab="Select">
             <form action="check_login.php" method="post">
                 タグの追加
                 <br><input type="text" id="intext"><br>
@@ -30,20 +30,20 @@
             </form>
             </div>
 
-        <div class="tab_panel-box tab_panel-box002" data-tab="Graph">
+        <div class="tab_panel_box tab_panel_box002" data-tab="Graph">
             <ul class="Graphmaterial">
                 <li><a href="BouGraph"></a>棒グラフ</li>
                 <li><a href="WariaiGraph"></a>割合円グラフ</li>
             </ul>
         </div>
 
-        <div class="tab_panel-box tab_panel-box003" data-tab="Week">
-          <p class="tab_panel-text">
+        <div class="tab_panel_box tab_panel_box003" data-tab="Week">
+          <p class="tab_panel_text">
             <div id="WeekCal"></div>
           </p>
         </div>
 
-        <div class="tab_panel-box tab_panel-box004" data-tab="Month">
+        <div class="tab_panel_box tab_panel_box004" data-tab="Month">
               <!-- The corrected placement of the div -->
               <div id="calender"></div>
       </div>
@@ -54,7 +54,7 @@
       'use strict';
     
       // DOM取得
-      const tabMenus = document.querySelectorAll('.tab_menu-item');
+      const tabMenus = document.querySelectorAll('.tab_menu_item');
       console.log(tabMenus);
     
       // イベント付加
@@ -70,28 +70,28 @@
         // クリックされた要素の親要素と、その子要素を取得
         const tabList = e.currentTarget.closest('.tab_menu');
         console.log(tabList);
-        const tabItems = tabList.querySelectorAll('.tab_menu-item');
+        const tabItems = tabList.querySelectorAll('.tab_menu_item');
         console.log(tabItems);
     
         // クリックされた要素の親要素の兄弟要素の子要素を取得
-        const tabPanelItems = tabList.nextElementSibling.querySelectorAll('.tab_panel-box');
+        const tabPanelItems = tabList.nextElementSibling.querySelectorAll('.tab_panel_box');
         console.log(tabPanelItems);
     
         // クリックされたtabの同階層のmenuとpanelのクラスを削除
         tabItems.forEach((tabItem) => {
-          tabItem.classList.remove('is-active');
+          tabItem.classList.remove('is_active');
         })
         tabPanelItems.forEach((tabPanelItem) => {
-          tabPanelItem.classList.remove('is-show');
+          tabPanelItem.classList.remove('is_show');
         })
     
         // クリックされたmenu要素にis-activeクラスを付加
-        e.currentTarget.classList.add('is-active');
+        e.currentTarget.classList.add('is_active');
     
         // クリックしたmenuのデータ属性と等しい値を持つパネルにis-showクラスを付加
         tabPanelItems.forEach((tabPanelItem) => {
           if (tabPanelItem.dataset.tab === tabTargetData) {
-            tabPanelItem.classList.add('is-show');
+            tabPanelItem.classList.add('is_show');
           }
         })
       }
